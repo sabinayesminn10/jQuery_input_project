d4ccbb135a2ce84eac0b4b8137ead479c69d22df
Sabina yesmin <sabinayesminn10@gmail.com>
2025-04-12 03:01:56 +0600

    use jQuery lib to creat it

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("#btnbtn").on({
+    click: function () {
+      //   var fastName = $("#first").val();
+      //   $("#inform_two").val("First Name: " + fastName);
+
+      //   $("#three").val()('email : + "#inform_two").text()');
+
+      //     $("#inform_three").text('Email: ' +);
+      //    ('Phone: ' + $("#four").val());
+
+      var name = "Address: " + $("#five").val();
+      $("#inform_five").text(name);
+    },
+  });
+  //   $("#inform_four").text;
+});
